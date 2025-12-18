56b618b58346a17523cb4fa617639d47373bf55d
berkaykeskin <keskinberkay30@gmail.com>
2025-12-19 00:52:18 +0300

    CreateRankEventPage.jsx And Ranking Page I added RankingsPage.jsx to search through rankings by actor and subject, the actor will be the currently logged in user in the future. Ranking CreateRankEventPage fetches the currently logged in user's ranking events. It should fetch by ranking type in the future. Then the user is able to drag and drop the users in this list and update the list.

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import LoginPage from "../Pages/LoginPage";
 import UserPostsPage from "../Pages/UserPostsPage";
 import RankingsPage from "../Pages/RankingsPage";
 import SignUpPage from "../Pages/SignUpPage";
+import CreateRankEventPage from "../Pages/CreateRankEventPage";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +15,8 @@ export const router = createBrowserRouter([
             { path: "login", element: <LoginPage /> },
             { path: "signup", element: <SignUpPage /> },
             { path: "user/posts", element: <UserPostsPage /> },
-            { path: "rankings", element: <RankingsPage /> }
+            { path: "rankings", element: <RankingsPage /> },
+            { path: "rank-event", element: <CreateRankEventPage />}
         ]
     },
     {
